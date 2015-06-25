3fa95243c808441ae40196bb86c24ebf3b090b54
Shane Martin <ghoulminy@live.com>
2015-06-25 20:55:37 +1000

    Syncing A way the Gravity Knuckles May work

---
+++ Assets/Scripts/Weapons/BaseWeapon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BaseWeapon : MonoBehaviour {
+	public PlayerController player;
+	public PlayerMovement playerMovement;
+	public CharacterController2D characterController2D;
+	public virtual void Awake(){
+		player = FindObjectOfType<PlayerController> ();
+		playerMovement = FindObjectOfType<PlayerMovement> ();
+		characterController2D = FindObjectOfType<CharacterController2D> ();
+	}
+    /// <summary>
+    /// Return true if the weapon is ready to fire
+    /// </summary>
+    public abstract bool isReady { get; }
+
+    /// <summary>
+    /// Tell the weapon to perform it's attack
+    /// </summary>
+    public abstract void Attack();
+}
---
+++ Assets/Scripts/Weapons/GravityKnuckles.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityKnuckles : BaseWeapon {
+	private float cooldown = 3;
+	private float gravityBoost = 6;
+	public GravCircle gravCircle;
+	private bool lerping = false;
+	private Vector3 endPos;
+
+	// Update is called once per frame
+	void Update () {
+		if (cooldown >= 0)
+			cooldown -= Time.deltaTime;
+	}
+	void FixedUpdate(){
+		if (lerping) {
+			movePlayer();
+		}
+	}
+
+	void movePlayer(){
+
+		player.transform.position = Vector3.Lerp (player.transform.position, endPos, 10 * Time.deltaTime);
+		Debug.Log ("Lerping");
+	}
+	void GravCircleHit(){
+		lerping = false;
+	}
+	public override void Attack ()
+	{
+		//Grabs the Right Stick (Up, Down, Left, Right) values.
+		float inputY = InControl.InputManager.ActiveDevice.RightStickY.Value;
+		float inputX = InControl.InputManager.ActiveDevice.RightStickX.Value;
+
+		//Puts these values into a Vector2 called force
+		Vector3 force = new Vector3(inputX, inputY, 0) * -gravityBoost;
+
+		//Update where the end position is
+		endPos = (force + player.transform.position);
+		gravCircle.transform.position = (endPos);
+		lerping = true;
+		//Remove Power from resource
+		//player.removePowerResource(value)
+		cooldown = 3;
+	}
+
+	public override bool isReady {
+		get {
+			return (cooldown <= 0);
+		}
+	}
+
+}
