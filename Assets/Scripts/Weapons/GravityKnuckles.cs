90de5d50df6c9a99232fa81d4a709f69b8e16af6
Shane Martin <ghoulminy@live.com>
2015-06-25 20:57:59 +1000

    Syncing to Master
    >> A Little Bit of Work
    
    >> Added a bit to the gravity knuckes

--- Assets/Scripts/Weapons/GravityKnuckles.cs
+++ Assets/Scripts/Weapons/GravityKnuckles.cs
@@ -4,9 +4,6 @@ using System.Collections;
 public class GravityKnuckles : BaseWeapon {
 	private float cooldown = 3;
 	private float gravityBoost = 6;
-	public GravCircle gravCircle;
-	private bool lerping = false;
-	private Vector3 endPos;
 
 	// Update is called once per frame
 	void Update () {
@@ -14,19 +11,8 @@ public class GravityKnuckles : BaseWeapon {
 			cooldown -= Time.deltaTime;
 	}
 	void FixedUpdate(){
-		if (lerping) {
-			movePlayer();
-		}
 	}
 
-	void movePlayer(){
-
-		player.transform.position = Vector3.Lerp (player.transform.position, endPos, 10 * Time.deltaTime);
-		Debug.Log ("Lerping");
-	}
-	void GravCircleHit(){
-		lerping = false;
-	}
 	public override void Attack ()
 	{
 		//Grabs the Right Stick (Up, Down, Left, Right) values.
@@ -36,10 +22,6 @@ public class GravityKnuckles : BaseWeapon {
 		//Puts these values into a Vector2 called force
 		Vector3 force = new Vector3(inputX, inputY, 0) * -gravityBoost;
 
-		//Update where the end position is
-		endPos = (force + player.transform.position);
-		gravCircle.transform.position = (endPos);
-		lerping = true;
 		//Remove Power from resource
 		//player.removePowerResource(value)
 		cooldown = 3;
