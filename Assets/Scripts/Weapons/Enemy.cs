50effe6e4e2f2e3742386bea81ba86543d8ddec2
Shane Martin <ghoulminy@live.com>
2015-06-29 13:51:21 +1000

    Doesn't Work so far, I'm gonna go home and see if I can fix it. Ill try looking at the player to find where collisions happen and how they are handled

--- Assets/Scripts/Weapons/Enemy.cs
+++ Assets/Scripts/Weapons/Enemy.cs
@@ -3,4 +3,16 @@ using System.Collections;
 
 public abstract class Enemy : MonoBehaviour {
 
+	public virtual void OnTriggerEnter(Collider collider){
+		notifyTarget (collider.gameObject);
+		Debug.Log ("Inside");
+	}
+	
+	public virtual void OnCollisionEnter(Collision collision){
+		notifyTarget (collision.gameObject);
+		Debug.Log ("Inside");
+	}
+
+	public abstract void notifyTarget(GameObject gameObject);
+
 }
--- Assets/Scripts/Weapons/ShredderMine.cs
+++ Assets/Scripts/Weapons/ShredderMine.cs
@@ -3,5 +3,20 @@ using System.Collections;
 
 public class ShredderMine : Enemy {
 
+	public float spikesToSpawn;
+	public GameObject spike;
 
+	public override void notifyTarget (GameObject gameObject)
+	{
+		Attack ();
+	}
+
+	void Attack(){
+		for (int i = 0; i < spikesToSpawn; i++) {
+			Vector3 random = Random.insideUnitSphere;
+			random.z = 0;
+			GameObject spikes = Instantiate(spike, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+			spikes.transform.localEulerAngles = random;
+		}
+	}
 }
