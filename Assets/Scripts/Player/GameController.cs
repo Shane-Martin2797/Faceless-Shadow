cce69d81a8d3794bf4c1270aab4b34038f0d202d
Shane Martin <ghoulminy@live.com>
2015-07-17 00:54:16 +1000

    Added Timer with minutes and seconds

--- Assets/Scripts/Player/GameController.cs
+++ Assets/Scripts/Player/GameController.cs
@@ -3,15 +3,31 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public static event System.Action<float> onTimerValueChange;
 
+	public float timer;
+	private float timerDefault = 240;
+	private bool gameover = false;
 
 	// Use this for initialization
 	void Awake () {
 		Application.LoadLevelAdditive (Scenes.Scene_HUD);
 	}
-	
+	void Start(){
+			timer = timerDefault;
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		if (!gameover) {
+			timer -= Time.deltaTime;
+			if(onTimerValueChange != null){
+				onTimerValueChange(timer);
+			}
+		}
+	}
+	void EarlyUpdate(){
+		if (timer <= 0) {
+			gameover = true;
+		}
 	}
 }
