1e7eb39428109e0229ac8b0a7b41312c0b93ee8f
Luke Wilmot <1001983@student.sae.edu.au>
2015-07-26 15:40:46 +1000

    Got the turret firing

--- Assets/Scripts/Enemy/TurretController.cs
+++ Assets/Scripts/Enemy/TurretController.cs
@@ -4,27 +4,47 @@ using System.Collections;
 public class TurretController : MonoBehaviour {
 	public GameObject player;
 	public Vector3 playerPos;
-
+	public float timeTillFire;
+	public float timeResetAmount;
+	public Rigidbody2D Bullet;
+	public float bulletSpeed;
 	void awake () {
 
 	}
 	// Use this for initialization
 	void Start () {
 		//playerPos = player.transform.localPosition;
+		timeTillFire = timeResetAmount;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		timeTillFire -= Time.deltaTime;
+		if (timeTillFire <= 0)
+		{
+			timeTillFire = timeResetAmount;
+			FireBullet();
+		}
+
 		playerPos = player.transform.position;
 		LookAtPlayer();
+
+
 		//this.transform.LookAt(playerPos);
 		//this.transform.Rotate(new Vector3(0,-90,0),Space.Self);
 	}
 
 	void LookAtPlayer()
 	{
-		this.transform.LookAt (playerPos);
-		this.transform.Rotate(new Vector3(0,-90,90),Space.Self);
+		this.transform.LookAt (playerPos); //follows the player 
+		this.transform.Rotate(new Vector3(0,-90,270),Space.Self); //rotates the turret so u can see it
+	}
+
+	void FireBullet()
+	{
+		Rigidbody2D bulletInstantiate;
+		bulletInstantiate = Instantiate (Bullet, this.transform.localPosition, this.transform.localRotation)as Rigidbody2D;
+		bulletInstantiate.rigidbody2D.AddForce (transform.up * bulletSpeed);
 	}
 }
